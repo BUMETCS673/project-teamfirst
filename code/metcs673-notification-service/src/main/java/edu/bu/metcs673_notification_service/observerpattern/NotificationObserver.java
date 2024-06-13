5c492d2e37184bb2d1930f82ea109bbfd382e0b0
small_J <90347002+zjzy1002@users.noreply.github.com>
2024-06-12 22:23:03 -0400

    Delete code/metcs673-notification-service/src/main/java/edu/bu/metcs673_notification_service/observerpattern directory

--- code/metcs673-notification-service/src/main/java/edu/bu/metcs673_notification_service/observerpattern/NotificationObserver.java
+++
@@ -1,6 +0,0 @@
-package edu.bu.metcs673_notification_service.observerpattern;
-
-public abstract class NotificationObserver {
-    public NotificationSubject subject;
-    public abstract void update();
-}
