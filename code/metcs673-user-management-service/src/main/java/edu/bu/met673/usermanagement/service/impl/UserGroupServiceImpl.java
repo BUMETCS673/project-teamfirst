a5937e0840a705fdd4e037259fc811a3f56e46af
ajordany <ajordany10@gmail.com>
2024-06-14 19:13:10 -0400

    Remove unnecessary files

--- code/metcs673-user-management-service/src/main/java/edu/bu/met673/usermanagement/service/impl/UserGroupServiceImpl.java
+++ code/metcs673-user-management-service/src/main/java/edu/bu/met673/usermanagement/service/impl/UserGroupServiceImpl.java
@@ -6,6 +6,9 @@ package edu.bu.met673.usermanagement.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.bu.met673.usermanagement.api.model.GroupDto;
+import edu.bu.met673.usermanagement.exceptions.ResourceNotFoundException;
+import edu.bu.met673.usermanagement.exceptions.ServiceException;
 import edu.bu.met673.usermanagement.repositories.GroupRepository;
 import edu.bu.met673.usermanagement.service.UserGroupService;
 
@@ -19,6 +22,18 @@ public class UserGroupServiceImpl implements UserGroupService {
 	
 	public UserGroupServiceImpl(@Autowired GroupRepository groupRepository) {
 		this.groupRepository = groupRepository;
+	}
+
+	@Override
+	public GroupDto createGroup(GroupDto groupDto) throws ServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GroupDto deleteGroup(Long groupId) throws ServiceException, ResourceNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
